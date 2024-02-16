f2418f1705f3c7d404c4ae362611485b4806c0e0
Shfnva <kristi.0260@gmail.com>
2024-02-16 22:01:11 +0100

    Update task-6.js

--- js/task-6.js
+++ js/task-6.js
@@ -1,38 +1,40 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, 0)}`;
+    .padStart(6, "0")}`;
 }
-const controls = document.getElementById('controls');
-const input = controls.querySelector('input');
-const createButton = controls.querySelector('[data-action="create"]');
-const destroyButton = controls.querySelector('[data-action="destroy"]');
-const boxesContainer = document.getElementById('boxes');
+const controls = document.getElementById("controls");
+const input = controls.querySelector("input");
+const createButton = controls.querySelector("[data-create]");
+const destroyButton = controls.querySelector("[data-destroy]");
+const boxesContainer = document.getElementById("boxes");
 
 function createBoxes() {
   const amount = Number(input.value);
   if (amount < 1 || amount > 100) {
-    alert('Please enter a number between 1 and 100.');
+    alert("Please enter a number between 1 and 100.");
     return;
   }
 
+  boxesContainer.innerHTML = "";
+
   const fragment = document.createDocumentFragment();
+  let size = 30;
   for (let i = 0; i < amount; i++) {
-    const size = 30 + i * 10;
-    const box = document.createElement('div');
+    const box = document.createElement("div");
     box.style.width = `${size}px`;
     box.style.height = `${size}px`;
     box.style.backgroundColor = getRandomHexColor();
-    fragment.append(box);
+    fragment.appendChild(box);
+    size += 10;
   }
-  boxesContainer.innerHTML = '';
-  boxesContainer.append(fragment);
+  boxesContainer.appendChild(fragment);
 
-  input.value = '';
+  input.value = "";
 }
 
 function destroyBoxes() {
-  boxesContainer.innerHTML = '';
+  boxesContainer.innerHTML = "";
 }
-createButton.addEventListener('click', createBoxes);
-destroyButton.addEventListener('click', destroyBoxes); // all ментор пише що правильно, я не розумію чому сайт видає помилку :(
+createButton.addEventListener("click", createBoxes);
+destroyButton.addEventListener("click", destroyBoxes);
